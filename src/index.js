0cdca9a9a8b5756c33f0bc5838bbe7bba3f99e3c
syam <syamsankuru@lyrostech.com>
2024-03-13 11:06:20 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-import React, { useContext } from 'react'
-import Home from './Home'
-import './Register.css'
-import { UserContext } from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export default function Register() {
-    const {flag,setFlag}=useContext(UserContext);
-  return (
-    <>
-    <div className='Register-model'>
-      <div className='Register-model-content'>
-        <div className="Register-header">
-          <div className="Register-signup-title">Register</div>
-          <div onClick={()=> setFlag(() => 0)} className="close"> &times;</div><br></br>
-        </div>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-       {/* <div>Register</div> */}
-        <input type="text" placeholder='username' required></input><br></br><br></br>
-        <input type="email" placeholder='email' requried></input><br></br><br></br>
-        <input type='password' placeholder='password' required></input><br></br><br></br>
-         <button onClick={() => setFlag((prevState) => 1)}>Submit</button>
-    </div>
-  </div>
-    </>
-  )
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
