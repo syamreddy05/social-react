8f8b853ddb9d0cbfb583a57013720967a0bec067
syam <syamsankuru@lyrostech.com>
2024-03-13 10:58:19 +0530

    social react commit

---
+++ src/Login.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { UserContext } from './App'
+import Home from './Home';
+import Register from './Register';
+
+
+export default function Login() {
+    const {flag, setFlag}=useContext(UserContext);
+// if (flag == 1) {
+//     <Home/>
+// } else if (flag == 2) {
+//   <> <Login/>
+//     <Register/></> 
+// } 
+  return (
+    <>
+    <div>Login</div>
+    <input type="text" placeholder='username' ></input><br></br>
+    <input type="password" placeholder='password'></input><br></br>
+    <button onClick={()=>setFlag((prev)=>2)}>Login</button>
+    <button onClick={()=>setFlag((prev)=>1)}>Create Account</button>
+    {flag===1 && (<Register />)}
+    </> 
+  )
+}
